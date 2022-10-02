dc39fdbc06675681b3a755af78d4bed023def98c
HairiBeast <shpetzla@gmail.com>
2022-10-02 16:59:31 +0100

    bugfix:completely reformatted javascript to work completely when deployed on Netlify

--- main.js
+++ main.js
@@ -3,48 +3,125 @@ function darkMode() {
   element.classList.toggle("dark-mode");
 }
 
-// let screen = document.getElementById("screen");
-// buttons = document.querySelectorAll("button.calc");
-// let screenValue = "";
-// for (item of buttons) {
-//   item.addEventListener("click", (e) => {
-//     buttonText = e.target.innerText;
-//     console.log("Button text is ", buttonText);
-//     if (buttonText == "x") {
-//       buttonText = "*";
-//       screenValue += buttonText;
-//       screen.value = screenValue;
-//     } else if (buttonText == "AC") {
-//       screenValue = "";
-//       screen.value = screenValue;
-//     } else if (buttonText == "=") {
-//       screen.value = eval(screenValue);
-//     } else {
-//       screenValue += buttonText;
-//       screen.value = screenValue;
-//     }
-//   });
-// }
+const calculate = (n1, operator, n2) => {
+  const firstNum = parseFloat(n1);
+  const secondNum = parseFloat(n2);
+  if (operator === "add") return firstNum + secondNum;
+  if (operator === "subtract") return firstNum - secondNum;
+  if (operator === "multiply") return firstNum * secondNum;
+  if (operator === "divide") return firstNum / secondNum;
+};
 
-let screen = document.getElementById("screen");
-buttons = document.querySelectorAll("button");
-let screenValue = "";
-for (item of buttons) {
-  item.addEventListener("click", (e) => {
-    buttonText = e.target.innerText;
-    console.log("Button text is ", buttonText);
-    if (buttonText == "x") {
-      buttonText = "*";
-      screenValue += buttonText;
-      screen.value = screenValue;
-    } else if (buttonText == "AC") {
-      screenValue = "";
-      screen.value = screenValue;
-    } else if (buttonText == "=") {
-      screen.value = eval(screenValue);
-    } else {
-      screenValue += buttonText;
-      screen.value = screenValue;
-    }
-  });
-}
+const getKeyType = (key) => {
+  const { action } = key.dataset;
+  if (!action) return "number";
+  if (
+    action === "add" ||
+    action === "subtract" ||
+    action === "multiply" ||
+    action === "divide"
+  )
+    return "operator";
+
+  return action;
+};
+
+const createResultString = (key, displayedNum, state) => {
+  const keyContent = key.textContent;
+  const keyType = getKeyType(key);
+  const { firstValue, operator, modValue, previousKeyType } = state;
+
+  if (keyType === "number") {
+    return displayedNum === "0" ||
+      previousKeyType === "operator" ||
+      previousKeyType === "calculate"
+      ? keyContent
+      : displayedNum + keyContent;
+  }
+
+  if (keyType === "decimal") {
+    if (!displayedNum.includes(".")) return displayedNum + ".";
+    if (previousKeyType === "operator" || previousKeyType === "calculate")
+      return "0.";
+    return displayedNum;
+  }
+
+  if (keyType === "operator") {
+    return firstValue &&
+      operator &&
+      previousKeyType !== "operator" &&
+      previousKeyType !== "calculate"
+      ? calculate(firstValue, operator, displayedNum)
+      : displayedNum;
+  }
+
+  if (keyType === "del") return 0;
+
+  if (keyType === "clear") return 0;
+
+  if (keyType === "calculate") {
+    return firstValue
+      ? previousKeyType === "calculate"
+        ? calculate(displayedNum, operator, modValue)
+        : calculate(firstValue, operator, displayedNum)
+      : displayedNum;
+  }
+};
+
+const updateCalculatorState = (
+  key,
+  calculator,
+  calculatedValue,
+  displayedNum
+) => {
+  const keyType = getKeyType(key);
+  const { firstValue, operator, modValue, previousKeyType } =
+    calculator.dataset;
+
+  calculator.dataset.previousKeyType = keyType;
+
+  if (keyType === "operator") {
+    calculator.dataset.operator = key.dataset.action;
+    calculator.dataset.firstValue =
+      firstValue &&
+      operator &&
+      previousKeyType !== "operator" &&
+      previousKeyType !== "calculate"
+        ? calculatedValue
+        : displayedNum;
+  }
+
+  if (keyType === "calculate") {
+    calculator.dataset.modValue =
+      firstValue && previousKeyType === "calculate" ? modValue : displayedNum;
+  }
+
+  if (keyType === "del") {
+    calculator.dataset.previousKeyType = "";
+  }
+
+  if (keyType === "clear") {
+    calculator.dataset.firstValue = "";
+    calculator.dataset.modValue = "";
+    calculator.dataset.operator = "";
+    calculator.dataset.previousKeyType = "";
+  }
+};
+
+const calculator = document.querySelector(".calculator");
+const display = calculator.querySelector(".calculator__result");
+const keys = calculator.querySelector(".calculator__body");
+
+keys.addEventListener("click", (e) => {
+  if (!e.target.matches("button")) return;
+  const key = e.target;
+  const displayedNum = display.textContent;
+  const resultString = createResultString(
+    key,
+    displayedNum,
+    calculator.dataset
+  );
+
+  display.textContent = resultString;
+  updateCalculatorState(key, calculator, resultString, displayedNum);
+});
